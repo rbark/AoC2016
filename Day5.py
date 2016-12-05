53e33941cc88f23512a95aee5f287ca0baa361f9
Rickard Jönsson <bark.rickard@gmail.com>
2016-12-05 18:17:08 +0100

    Fag 5 del 2 klar

--- Day5.py
+++ Day5.py
@@ -2,13 +2,22 @@ import hashlib
 
 s = hashlib.md5("abc5017308".encode('utf-8')).hexdigest()
 
+position_set = set()
 startkey = 'uqwqemis'
 count = 0
 number_str = ''
+chars = ['', '', '', '', '', '', '', '']
 
-while len(number_str) != 8 :
+while len(position_set) != 8:
     s = str(hashlib.md5((str(startkey) + str(count)).encode('utf-8')).hexdigest())
     if s[:5] == str('00000'):
-        number_str += number_str.join(s[5:6])
+        if s[5:6].isdigit() and int(s[5:6]) < 8:
+            if s[5:6] not in position_set:
+                position_set.add(s[5:6])
+                chars[int(s[5:6])] = s[6:7]
     count += 1
-print(number_str)
+
+answer = ''
+for c in chars:
+    answer += c
+print(answer)
