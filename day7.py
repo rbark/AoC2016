960c7ecfd823c4cf6e56d528983bd8279f3ccb05
Rickard Jönsson <bark.rickard@gmail.com>
2016-12-08 00:17:16 +0100

    Dag7 del2

--- day7.py
+++ day7.py
@@ -1,6 +1,8 @@
 import re
 
 hypernet_pattern = '\[[a-z]*\]'
+cnt = 0
+cnt2 = 0
 
 
 def get_parts(word):
@@ -22,7 +24,19 @@ def check_if_abba(word):
     return False
 
 
-def valid_ip(hypernets, nets):
+def check_if_aba(word):
+    i = 0
+    words = []
+    valid = False
+    while i < len(word) - 2:
+        if word[i] == word[i + 2] and word[i] != word[i + 1]:
+            valid = True
+            words.append( word[i] + word[i + 1] + word[i + 2])
+        i += 1
+    return valid, words
+
+
+def valid_ip(nets, hypernets):
     for word in hypernets:
         if check_if_abba(word):
             return False
@@ -32,13 +46,24 @@ def valid_ip(hypernets, nets):
     return False
 
 
+def valid_ip_aba(nets, hypernets):
+    for word in nets:
+        (valid, words) = check_if_aba(word)
+        if valid:
+            for word in words:
+                word_to_find = word[1] + word[0] + word[1]
+                for hypernet in hypernets:
+                    if word_to_find in hypernet:
+                        return True
+    return False
 
-cnt = 0
 with open('day7.txt') as file:
     rows = file.readlines()
     for word in rows:
-        (nets , hypernets ) = get_parts(word)
-        if valid_ip(hypernets, nets):
+        (nets, hypernets) = get_parts(word)
+        if valid_ip(nets, hypernets):
             cnt += 1
+        if valid_ip_aba(nets, hypernets):
+            cnt2 += 1
 print(cnt)
-
+print(cnt2)
