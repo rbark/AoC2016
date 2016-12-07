cf4ea06b7ae97b04d577f101a2257b7abbf844ae
Rickard Jönsson <bark.rickard@gmail.com>
2016-12-07 19:17:23 +0100

    Dag 6 båda delarna klara

--- Day6.py
+++ Day6.py
@@ -1,54 +1,25 @@
+letter_freq = [{} for i in range(8)]  # array with 8 dicts
 
 with open('day6.txt') as file:
     instruction_rows = file.readlines()
+    for word_count, word in enumerate(instruction_rows):
+        # print(str(word_count) + ': ' + word)
+        for i, c in enumerate(word.strip()):
+            if c in letter_freq[i].keys():
+               letter_freq[i][c] += 1
+            else:
+               letter_freq[i][c] = 1
 
-col1 = []
-col2 = []
-col3 = []
-col4 = []
-col5 = []
-col6 = []
-col7 = []
-col8 = []
+max_string = ''
+min_string = ''
 
-def put_letters_to_array(word):
-    col1.append(word[0])
-    col2.append(word[1])
-    col3.append(word[2])
-    col4.append(word[3])
-    col5.append(word[4])
-    col6.append(word[5])
-    col7.append(word[6])
-    col8.append(word[7])
+for i in range(8):
+    for c, val in letter_freq[i].items():
+        if val == max(letter_freq[i].values()):
+            max_string += c
+        if val == min(letter_freq[i].values()):
+            min_string += c
 
-def max_chars(word):
-    counts = {}
-    for char in word:
-        if char in counts.keys():
-            counts[char] += 1
-        else:
-            counts[char] = 1
+print(max_string)
+print(min_string)
 
-
-    max_val = max(counts.values())
-
-    for c, val in counts.items():
-        if val == max_val:
-            return c
-
-
-
-sec_word = ''
-for line in instruction_rows:
-    put_letters_to_array(line)
-
-sec_word += max_chars(col1)
-sec_word += max_chars(col2)
-sec_word += max_chars(col3)
-sec_word += max_chars(col4)
-sec_word += max_chars(col5)
-sec_word += max_chars(col6)
-sec_word += max_chars(col7)
-sec_word += max_chars(col8)
-
-print( sec_word)
